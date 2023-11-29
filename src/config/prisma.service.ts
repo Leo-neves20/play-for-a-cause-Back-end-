ebe18cf73d84006d365147a54ace04d5052d4c52
leonadoNeves <leonardo.neves@dbit.srv.br>
2023-11-29 15:34:28 -0300

    adciona configuração com o banco postgres utilizando o docker

---
+++ src/config/prisma.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    this.$connect();
+  }
+  $connect(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
